9757300ce4425978deb4058e866324d231c8bfe7
Nasreen Arif <nasreen_arif@live.com>
2025-01-25 22:16:19 -0500

    JS Overview

---
+++ HTML&CSSOverview/script.js
@@ -0,0 +1,48 @@
+// alert('Hello Class!');
+
+
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello World!");
+
+let userMessage="Hello Class!";
+
+// console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+
+
+// const a=12;
+
+// console.log(a);
+
+// a=20;
+
+// console.log(a);
+
+
+
+let name=33;
+
+// console.log(typeof name);
+
+
+let user={
+    name:'Nasreen',
+    age:33
+}
+
+// console.log(user.name);
+
+
+const hobbies=['Sports','Cooking','Reading'];
+
+// console.log(hobbies[1]);
+
+
+import { apiKey1 } from "./apikey.js";
+
+console.log(apiKey1);
