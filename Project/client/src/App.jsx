414e6e3f05cc2caa421b0bcd1ef060d226d601dc
Nasreen Arif <nasreen_arif@live.com>
2025-02-04 19:15:49 -0500

    components

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [email, setEmail] =useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
+    <Login></Login>
+    {/* <Courses title="CS418" description="Web Programming">
+      <h3>React Class - Project Setup</h3>
+      <p>Testing children props</p>
+
+    </Courses>
+
+    
+    <Courses title="CS471" description="Operating System"></Courses>
+    
+    <Courses title="CS432" description="Web Science"></Courses>
+     <Images></Images>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -27,7 +33,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ Project/client/src/components/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./Login.css";
+
+export default function Login() {
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+    const [submitted, setSubmitted] = useState(false);  
+
+    function handleInputChange(identifier, value) {
+        if (identifier === "email") {
+            setEnteredEmail(value);
+        } else {
+            setEnteredPassword(value);
+        }
+    }
+
+    const handleLogin =  () => {
+        setSubmitted(true);  
+        if(enteredEmail!="")      
+        alert('Email:' +enteredEmail);
+                
+    };
+
+    const emailNotValid = submitted && !enteredEmail.includes("@");
+    const passwordNotValid = submitted && enteredPassword.trim().length < 8;
+
+    return (
+        <div id="login">                        
+            <div className="controls">
+                <p>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        className={emailNotValid ? "invalid" : undefined}
+                        onChange={(event) => handleInputChange("email", event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className={passwordNotValid ? "invalid" : undefined}
+                        onChange={(event) =>
+                            handleInputChange("password", event.target.value)
+                        }
+                    />
+                </p>
+                
+            </div>
+            <div className="actions">
+                <button type="button" className="button">
+                    Create a new account
+                </button>
+                <button className="button" onClick={handleLogin}>
+                    Sign In
+                </button>
+            </div>
+        </div>
+    );
+}
